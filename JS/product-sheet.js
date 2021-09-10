be3418b1922e2e4a9a5a2e9ead819a892de38872
Amal-Igor <83766830+Amal-Igor@users.noreply.github.com>
2021-09-10 17:07:27 +0200

    Fiche produit

--- JS/product-sheet.js
+++ JS/product-sheet.js
@@ -1,19 +1,56 @@
 
-   /* const getJsonData = fetch('http://localhost:3000/api/cameras').then(response => response.json())*/
-    
+
+
+const getJsonData = fetch('http://localhost:3000/api/cameras')
+.then(response => response.json())
+.then (getId => {
+
+    const products = getId;
+
+    for(let id in products){
+
+    let getHref = window.location.href
+    let n = getHref.substring(getHref.indexOf('id=') + 3)
+    let jsonId = getJsonData.id
+
+        if (n = jsonId) {
+            console.log(getJsonData.id[n])
+        }
+        return 
+}
+})
+
+
 
 
     
+
+/*
+
+function generateProductSheet(){
+    return new Promise((generateCustomInfos, showErrorMessage) => {
+        let getHref = window.location.href
+        let n = getHref.substring(getHref.indexOf('id=') + 3)
+        const getJsonData = fetch('http://localhost:3000/api/cameras')
+        .then(response => response.json())
+        .then (data => {
+        getJsonData.id = console.log(data[0])
+    })
+        if ()
+    })
+} 
             
+*/
 
 
 
 
-
-
+/*
 let getHref = window.location.href
 
-let n = getHref.substring(getHref.indexOf('id=') + 3);
+let n = getHref.substring(getHref.indexOf('id=') + 3)
+
+
 
 
 /*
