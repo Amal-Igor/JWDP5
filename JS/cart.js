062b4ee1f1fdb16f9aea2a9c70f64253c9cbb5e7
Amal-Igor <83766830+Amal-Igor@users.noreply.github.com>
2021-09-30 16:01:52 +0200

    /////Test Validation formulaire//////

--- JS/cart.js
+++ JS/cart.js
@@ -25,16 +25,21 @@ else {
     let hydrateCart = [];
 
     for (x = 0; x < productLocalStorage.length; x++) {
+
+       
+
         hydrateCart = hydrateCart + 
 
         `<div class="row container-fluid justify-content-center text-center">
             <div class="product-name cart-element col "> ${productLocalStorage[x].nom}</div>
             <div class="product-option cart-element col"> ${productLocalStorage[x].option}</div>
-            <div class="product-quantity cart-element col">Quantité</div>
             <div class="product-price cart-element col">${productLocalStorage[x].prix} €</div>
             <button type="button" class="product-delete cart-element col">Bouton Supprimer</button>   
         </div>
         `;
+
+
+   
     }
         if(x == productLocalStorage.length){
             ///Calcul du prix total du panier
@@ -48,8 +53,7 @@ else {
         const reducer = (accumulator, currentValue) => accumulator + currentValue;
         const totalPrice = calculPrice.reduce(reducer, 0);
         */
-
-        
+       
         const totalPrice = productLocalStorage.map(montant => montant.prix).reduce((a, b) => a + b, 0)
         console.log(totalPrice);
 
@@ -57,13 +61,17 @@ else {
         reachMainContainer.innerHTML = hydrateCart + 
         `<div class="row container-fluid justify-content-center text-center">
             <div class="total-amount col">Prix total : ${totalPrice} €</div>
-            <div class="order col">yo</div>
+            
+            <button id="order-cart-button" class="order col">Passer la commande</button>
         </div>`;
         }    
 }
 
+///BOUTON POUR VIDER LE PANIER 
 
-      //Ajout du bouton supprimer
+ /*<button id="empty-cart-button" class="empty-cart col">Vider le panier</button>*/ 
+
+//Ajout du bouton supprimer
 let reachDeleteButton = document.querySelectorAll(".product-delete");
       console.log(reachDeleteButton);
         //Creation d'un tableau vide
@@ -80,31 +88,80 @@ let reachDeleteButton = document.querySelectorAll(".product-delete");
                     tab = productLocalStorage
                 let response = confirm("Vous allez retirer ce produit de votre panier, voulez-vous continuer?")
 
-                if (response == true ) {
-                    tab.splice(getIndexOnLocalStorage, 1)
-                    productLocalStorage = localStorage.setItem("product", JSON.stringify(tab));
-                    window.location.href ="cart.html";}
+                    if (response == true ) {
+                        tab.splice(getIndexOnLocalStorage, 1)
+                        productLocalStorage = localStorage.setItem("product", JSON.stringify(tab));
+                        window.location.href ="cart.html";}
 
-                else {
-                }
+                    else {
+                    }
 
           })
-              }
+    }
+
+//Template du formulaire
+const goToForm = () => {
+
+    const reachFormContainer = document.getElementById("form-sub-container");
+
+    const formScheme = `<form class="form-order">
+    <label for="nom">Nom :</label>
+      <input class="input-entry" id="input-nom" name="nom" type="text" required>            
+    <label for="prenom">Prénom :</label>
+        <input class="input-entry" id="input-prenom" name="prenom" type="text" required>
+    <label for="mail">Email :</label>
+         <input class="input-entry" id="input-mail" name="mail" type="text" required>
+    <label for="adresse">Adresse :</label>
+        <textarea class="input-entry" id="input-adresse" name="adresse" type="text" required></textarea>
+     <label for="ville">Ville :</label>
+        <input class="input-entry" id="input-ville" name="ville" type="text" required>
+    
+    <button type=""submit" id="finalize-order">Valider votre commande</button>
+</form>`
+
+    reachFormContainer.innerHTML = formScheme;
+    }
 
 
-/*
-              let getIdToDelete = productLocalStorage.indexOf(productLocalStorage[i]);
-              console.log(getIdToDelete);
-                    if(getIdToDelete === productLocalStorage[i]){
-                        productLocalStorage.splice(i, 1);
-                    }
-              
-
-
-                       console.log(event)
-
-                productLocalStorage = productLocalStorage.filter(element => element)
+///Ajout de l'évènement pour générer le formulaire au click
+let reachOrderButton = document.getElementById("order-cart-button");
+    reachOrderButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        goToForm();
+    })
 
 
 
-              */
+///------  Récupération des données du formulaire  --------------\\\
+let reachFinalizeOrder = document.getElementById("finalize-order")
+reachFinalizeOrder.addEventListener("click",  (event)=>{
+event.preventDefault();
+
+
+localStorage.setItem("prenom" ,document.getElementById("input-prenom").value);
+localStorage.setItem("nom" ,document.getElementById("input-nom").value);
+localStorage.setItem("" ,document.getElementById("input-adresse").value);
+localStorage.setItem("" ,document.getElementById("input-mail").value);
+localStorage.setItem("" ,document.getElementById("input-ville").value);
+})
+        
+
+
+
+/////Test Validation formulaire//////
+
+
+
+
+/* A RECUPERER
+          //Mise en place des quantités
+         
+          for(l = 1; l <= 10; l++){
+            let reachQuantity = document.querySelectorAll(".product-quantity");
+        let createQuantityDropDown = document.createElement("option");
+        reachQuantity.appendChild(createQuantityDropDown);
+
+            
+        }
+*/
+    
